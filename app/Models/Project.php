683e0bf3c41edd28bc586fe7adaf62f3137a2b46
osaris <raphael.emourgeon@gmail.com>
2020-09-30 19:17:36 +0000

    Add project model.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
+class Project extends Model
 {
     use HasFactory;
 
-    public function project()
+    public function tasks()
     {
-        return $this->belongsTo('App\Models\Project');
+        return $this->hasMany('App\Models\Task');
     }
 }
