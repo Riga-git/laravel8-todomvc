ad94be7a00fc969ebf87ab2a5ee4334644095ea6
Riga-git <luca.rigazzi@gmail.com>
2020-10-08 17:42:54 +0000

    config db heroku

--- config/filesystems.php
+++ config/filesystems.php
@@ -65,6 +65,19 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'heroku' => [
+        'driver' => 'pgsql',
+        'host' => parse_url(getenv("DATABASE_URL"))["host"],
+        'database' => substr(parse_url(getenv("DATABASE_URL"))["path"],1),
+        'username' => parse_url(getenv("DATABASE_URL"))["user"],
+        'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+        'charset' => 'utf8',
+        'prefix' => '',
+        'schema' => 'public',
+        ],
+
+
+
     ],
 
     /*
